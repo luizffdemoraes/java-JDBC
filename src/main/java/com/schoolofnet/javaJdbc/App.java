d368b6632d00df02318464576ffbf52a67dfa108
Luiz Felipe Moraes <65265977+Deehferreira@users.noreply.github.com>
2022-01-26 20:22:20 -0300

    Melhorando nossa classe de conexão

--- src/main/java/com/schoolofnet/javaJdbc/App.java
+++ src/main/java/com/schoolofnet/javaJdbc/App.java
@@ -1,14 +1,14 @@
 package com.schoolofnet.javaJdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
 
-        new ConnectionFactory().getConnection();
 
+        ConnectionFactory.getConnection();
+
+//        new ConnectionFactoryOld().getConnection();
 //        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?useLegacyDatatimeCode=false&serverTimeZone=UTC", "root", "root");
         System.out.println("Connected");
 
--- src/main/java/com/schoolofnet/javaJdbc/ConnectionFactory.java
+++ src/main/java/com/schoolofnet/javaJdbc/ConnectionFactory.java
@@ -6,24 +6,32 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private Connection con = null;
+    private static ConnectionFactory instance = new ConnectionFactory();
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root";
+    public static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
-    public Connection getConnection() {
+    private ConnectionFactory() {
+        // This is deprecated, The driver is automatically
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?useLegacyDatatimeCode=false&serverTimeZone=UTC", "root", "root");
+            Class.forName(DRIVER_CLASS);
+        } catch (ClassNotFoundException ex) {
+            ex.getMessage();
+        }
+    }
+
+    private Connection createConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+            ex.getMessage();
         }
         return con;
     }
 
-    public void closeConnection() {
-        try {
-            if (con != null) {
-                con.close();
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+    public static Connection getConnection() {
+        return instance.createConnection();
     }
 }
