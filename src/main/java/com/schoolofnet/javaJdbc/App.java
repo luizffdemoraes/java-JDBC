b456c034eb04954d57cd750c9d8b302354998785
Luiz Felipe Moraes <65265977+Deehferreira@users.noreply.github.com>
2022-01-26 19:12:48 -0300

    Criando conexão

--- src/main/java/com/schoolofnet/javaJdbc/App.java
+++ src/main/java/com/schoolofnet/javaJdbc/App.java
@@ -1,8 +1,18 @@
 package com.schoolofnet.javaJdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello word!");
+    public static void main(String[] args) throws SQLException {
+
+        new ConnectionFactory().getConnection();
+
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?useLegacyDatatimeCode=false&serverTimeZone=UTC", "root", "root");
+        System.out.println("Connected");
+
+
     }
 
 }
---
+++ src/main/java/com/schoolofnet/javaJdbc/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package com.schoolofnet.javaJdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private Connection con = null;
+
+    public Connection getConnection() {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?useLegacyDatatimeCode=false&serverTimeZone=UTC", "root", "root");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+
+    public void closeConnection() {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
