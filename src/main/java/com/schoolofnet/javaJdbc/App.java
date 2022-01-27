53066bedbe3715fe31fbd6a8e96ed5d6d55e08d9
Luiz Felipe Moraes <65265977+Deehferreira@users.noreply.github.com>
2022-01-26 22:12:34 -0300

    Prepared Statement

--- src/main/java/com/schoolofnet/javaJdbc/App.java
+++ src/main/java/com/schoolofnet/javaJdbc/App.java
@@ -1,5 +1,6 @@
 package com.schoolofnet.javaJdbc;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class App {
@@ -11,6 +12,16 @@ public class App {
 //        new ConnectionFactoryOld().getConnection();
 //        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?useLegacyDatatimeCode=false&serverTimeZone=UTC", "root", "root");
         System.out.println("Connected");
+
+        // Trabalhar com QUERY
+        // PreparedStatement é indicado para operações de DML
+        // Statement indicado para comando DDL é mais lento
+        String query = "SELECT * FROM users WHERE ID = ?";
+
+        PreparedStatement preparedStatement = ConnectionFactory.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1, 5);
+        preparedStatement.executeQuery();
+
 
 
     }
