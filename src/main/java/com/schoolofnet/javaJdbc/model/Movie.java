3bd29745874c327047e9e2aa6d409f9e67c5a0b8
Luiz Felipe Moraes <65265977+Deehferreira@users.noreply.github.com>
2022-02-02 00:29:10 -0300

    Criando model movie

---
+++ src/main/java/com/schoolofnet/javaJdbc/model/Movie.java
@@ -0,0 +1,33 @@
+package com.schoolofnet.javaJdbc.model;
+
+public class Movie {
+
+    private Integer id;
+
+    private String name;
+
+    public Movie() {
+        
+    }
+
+    public Movie(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
